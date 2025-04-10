1dec56d2a735b5921ae933d250dc4ad63836ba4c
Rohit Gupta <gupta.rohitg.rohit900@gmail.com>
2025-04-10 18:14:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,5 +144,5 @@ app.layout = dbc.Container([
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8050))
+    port = int(os.environ.get("PORT", 80))
     app.run(host='0.0.0.0', port=port)
